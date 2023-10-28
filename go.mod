23895a987cfddfbf39f42364461811a5dc22ec5a
ankesh <ankeshnirala@gmail.com>
2023-10-28 15:40:34 +0530

    status enum implementation done

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ankeshnirala/go-enums
+
+go 1.21.3
--- main.go
+++ main.go
@@ -1,1 +1,23 @@
+package main
 
+import (
+	"fmt"
+	"reflect"
+)
+
+type Status int
+
+const (
+	Delete Status = iota + 1
+	Active
+	Blocked
+)
+
+func (s Status) String() string {
+	return [...]string{"Delete", "Active", "Blocked"}[s-1]
+}
+
+func main() {
+	var status = Delete
+	fmt.Println(reflect.TypeOf(status.String()))
+}
